948f5983b6d0321cee891b77c9bdf046e6617959
RomanGorovtsov <gorovtsovroman@gmail.com>
2025-11-11 16:44:21 +0300

    1. Перенес создание пользователя в класc User 2. Переделал хвостики ендпоинтов в запросах в переменные 3. Добавил метод getFullEndPoint. 4. baseUrl в конфиги

--- src/test/java/api/models/User.java
+++ src/test/java/api/models/User.java
@@ -3,6 +3,9 @@ package api.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 public class User {
@@ -22,4 +25,25 @@ public class User {
     private String state;
     private String city;
     private String mobileNumber;
+
+    public Map<String, String> toFormParamsMapWithFullUserData() {
+        Map<String, String> formParams = new HashMap<>();
+        formParams.put("name", this.name);
+        formParams.put("email", this.email);
+        formParams.put("password", this.password);
+        formParams.put("title", this.title);
+        formParams.put("birth_day", this.birthDay);
+        formParams.put("birth_month", this.birthMonth);
+        formParams.put("birth_year", this.birthYear);
+        formParams.put("firstname", this.firstName);
+        formParams.put("lastname", this.lastName);
+        formParams.put("company", this.company);
+        formParams.put("address1", this.address1);
+        formParams.put("country", this.country);
+        formParams.put("zipcode", this.zipcode);
+        formParams.put("state", this.state);
+        formParams.put("city", this.city);
+        formParams.put("mobile_number", this.mobileNumber);
+        return formParams;
+    }
 }
